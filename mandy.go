4051c45b3d9b54a812dbae8a5b0b685316276826
yarbelk <james.rivettcarnac@gmail.com>
2015-03-24 10:32:29 +0800

    change limits and add git ignore

--- mandy.go
+++ mandy.go
@@ -6,25 +6,26 @@ import (
 	"math"
 	"os"
 	"fmt"
-	"errors"
+	"sync"
 )
 
 var outputChars [16]tb.Cell = [16]tb.Cell{
-	tb.Cell{'\u2592', 1, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 1*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 2*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 3*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 4*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 5*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 6*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 7*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 8*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 9*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 10*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 11*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 13*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 14*13, tb.ColorBlack},
-	tb.Cell{'\u2592', 1 + 15*13, tb.ColorBlack},
+	tb.Cell{'\u2592', 0, tb.ColorBlack},
+	tb.Cell{'\u2592', 1*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 2*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 3*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 4*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 5*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 6*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 7*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 8*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 9*18, tb.ColorBlack},
+	tb.Cell{'\u2592', 10*18, tb.ColorBlack},
+}
+
+type screenPoint struct {
+	x, y int
+	point complex128
 }
 
 type mandelState struct {
@@ -32,55 +33,58 @@ type mandelState struct {
 	limit int32
 }
 
-var (
-	RADIUS float64 = 2.0
-	YSTEP float64 = 0.05
-	XSTEP float64 = 0.01
-)
-
-
-func convergantPoint(mandyVal *complex128) bool {
-	return (cmplx.Abs(*mandyVal) < RADIUS)
+func (m* mandelState)convergantPoint(mandyVal *complex128) bool {
+	return (cmplx.Abs(*mandyVal) < m.radius)
 }
 
-func (m *mandelState) mandelbrot(point complex128) (distance int32, err error) {
+func (m *mandelState) mandelbrot(pointChan <-chan screenPoint, wg sync.WaitGroup) {
+	defer wg.Done()
+	var distance int32
 	if m.limit <= 0 {
-		return 0, errors.New("Mandy needs a max depth to recurse to")
+		os.Exit(1)
 	}
-	var currentVal complex128 = point;
-
-	for distance=0; distance <= m.limit; distance++ {
-		currentVal = cmplx.Pow(currentVal, 2.0+0i) + point
-		if ! convergantPoint(&currentVal) {
-			return distance, nil
+	
+	for point := range pointChan {
+		var currentVal complex128 = point.point
+		for distance=0; distance <= m.limit; distance++ {
+			currentVal = cmplx.Pow(currentVal, 2.0+0i) + point.point
+			if ! m.convergantPoint(&currentVal) {
+				m.printMandyPoint(distance, point.x, point.y)
+			}
 		}
 	}
-	return distance, nil
 }
 
-func (mandy *mandelState) setupMandy(termX, termY int) {
-	mandy.yStep = (mandy.yMax - mandy.yMin) / float64(termY)
-	mandy.xStep = (mandy.xMax - mandy.xMin) / float64(termX)
+func (m *mandelState) printMandyPoint(distance int32, x, y int) {
+	i := int32(math.Min(15, math.Max(0, math.Log2(float64(distance)))))
+	cell := outputChars[i]
+	tb.SetCell(x, y, cell.Ch, cell.Fg, cell.Bg)
+}
+
+
+func (m *mandelState) setupMandy(termX, termY int) {
+	m.yStep = (m.yMax - m.yMin) / float64(termY)
+	m.xStep = (m.xMax - m.xMin) / float64(termX)
 }
 
 func (mandy *mandelState) mandyRun(termX, termY int) {
 	var yCur, xCur float64
 	var point complex128
+	var wg sync.WaitGroup
 
+	pointChan := make(chan screenPoint)
+	wg.Add(1)
+	go mandy.mandelbrot(pointChan, wg)
 	for y := 0 ; y < termY; y++ {
 		for x := 0 ; x < termX; x++ {
 			xCur = mandy.xMin + (mandy.xStep * float64(x))
 			yCur = mandy.yMin + (mandy.yStep * float64(y))
 			point = complex(xCur, yCur)
-			distance, err := mandy.mandelbrot(point)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-			i := int32(math.Min(15, math.Max(0, math.Log2(float64(distance)))))
-			cell := outputChars[i]
-			tb.SetCell(x, y, cell.Ch, cell.Fg, cell.Bg)
+			pointChan <- screenPoint{x: x, y: y, point: point}
 		}
 	}
+	close(pointChan)
+	wg.Wait()
 	tb.SetCursor(0, 0)
 	tb.Flush()
 	tb.HideCursor()
@@ -89,11 +93,11 @@ func (mandy *mandelState) mandyRun(termX, termY int) {
 
 func main() {
 	var mandy *mandelState = &mandelState{
-		xMin: -1.0,
-		yMin: 0.0,
-		xMax: 0.25,
-		yMax: 0.5,
-		limit: 65536,
+		xMin: -2.0,
+		yMin: -1.0,
+		xMax: 0.5,
+		yMax: 1.0,
+		limit: 4096,
 		radius: 2.3,
 	}
 
